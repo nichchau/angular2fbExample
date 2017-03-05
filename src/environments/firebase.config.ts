092498fd9dbdf2ade629c53f6d2455935419c5f9
nichchau818 <nichchau@gmail.com>
2017-03-05 22:43:11 +0800

    first commit

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBETqAec-vFJR4SyBxbh63AvHdnmWykQvc",
+    authDomain: "angular2test-7b681.firebaseapp.com",
+    databaseURL: "https://angular2test-7b681.firebaseio.com",
+    storageBucket: "angular2test-7b681.appspot.com"
+};
